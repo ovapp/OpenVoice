5fa42cd80c2f8c0e7e1ddfec3d0c926411d36dab
VerbalThree <verbalthree00@gmail.com>
2025-01-01 05:33:00 -0300

    Criando a página de Onboarding

--- openvoice/app/page.tsx
+++ openvoice/app/page.tsx
@@ -1,101 +1,119 @@
-import Image from "next/image";
+import BlackPointImage from "@/components/bpimage";
+import { inter, sourceSerif4 } from "@/app/layout";
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="relative flex min-h-screen flex-col items-center">
+      <div className="h-screen w-screen flex items-center justify">
+        <div id="authform" className="px-3 space-y-3">
+          {/* Logo */}
+          <div
+            id="headerWrapper"
+            className="relative mb-20 bottom-60 min-[320px]:bottom-36"
+          >
+            <div className="header scale-150" role="banner">
+              <div className="items-center mx-auto text-center justify-center w-20 max-[320px]:hidden">
+                <BlackPointImage />
+              </div>
+            </div>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <div
+            id="authFormHeader"
+            className="relative p-0 bottom-56 min-[320px]:bottom-32"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {/* Name */}
+            <h1
+              id="authHeaderTop"
+              className={cn("mb-2 scale-150", inter.className)}
+            >
+              OpenVoice
+            </h1>
+
+            {/* Slogan */}
+            <h1
+              id="authHeaderBottom"
+              className={cn("scale-90", sourceSerif4.className)}
+            >
+              Giving voice for the people.
+            </h1>
+
+            <div className="relative top-24 min-[320px]:top-12">
+              <LoginButton>
+                <Button
+                  variant="default"
+                  id="buttonFont"
+                  className={cn(
+                    "w-[280px] h-[43px] px-20 py-13 mt-5 rounded-xl gap-8",
+                    inter.className
+                  )}
+                >
+                  Create an Account
+                </Button>
+              </LoginButton>
+              <LoginButton>
+                <Button
+                  variant="default"
+                  id="buttonFontSecondary"
+                  className={cn(
+                    "w-[280px] h-[43px] px-20 py-13 mt-5 rounded-xl gap-8",
+                    inter.className
+                  )}
+                >
+                  Sign In
+                </Button>
+              </LoginButton>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className="relative flex w-full max-w-[32rem] flex-1 flex-col justify-center gap-y-6 lg:max-w-[50rem] bottom-16">
+        <footer className="flex justify-between">
+          <p id="ovapp" className="relative">
+            © 2025 ovapp
+          </p>
+          <ul id="list" className="flex gap-2" role="list">
+            <li className="flex items-center gap-2 after:size-[1.5px] after:rounded-full after:bg-gray-1100 last:after:hidden">
+              <a className="relative rounded-sm text-gray-1000 hover:text-gray-1100 after:absolute after:-inset-x-1.5 after:-inset-y-0.5 after:rounded-sm after:border-2 after:border-blue-500 after:opacity-0 focus-visible:after:opacity-100">
+                Sobre
+              </a>
+            </li>
+            <li className="flex items-center gap-2 after:size-[1.5px] after:rounded-full after:bg-gray-1100 last:after:hidden">
+              <a className="relative rounded-sm text-gray-1000 hover:text-gray-1100 after:absolute after:-inset-x-1.5 after:-inset-y-0.5 after:rounded-sm after:border-2 after:border-blue-500 after:opacity-0 focus-visible:after:opacity-100">
+                GitHub
+              </a>
+            </li>
+            <li className="flex items-center gap-2 after:size-[1.5px] after:rounded-full after:bg-gray-1100 last:after:hidden">
+              <a className="relative rounded-sm text-gray-1000 hover:text-gray-1100 after:absolute after:-inset-x-1.5 after:-inset-y-0.5 after:rounded-sm after:border-2 after:border-blue-500 after:opacity-0 focus-visible:after:opacity-100">
+                Docs
+              </a>
+            </li>
+            <li className="flex items-center gap-2 after:size-[1.5px] after:rounded-full after:bg-gray-1100 last:after:hidden">
+              <Select>
+                <SelectTrigger className="relative rounded-xl text-gray-1000 hover:text-gray-1100 after:absolute after:-inset-x-1.5 after:-inset-y-0.5 after:rounded-sm after:border-2 after:border-blue-500 after:opacity-0 focus-visible:after:opacity-100 w-[85px]">
+                  <SelectValue placeholder="Language" />
+                </SelectTrigger>
+                <SelectContent id="languageSelect" className="rounded-xl">
+                  <SelectItem value="en">English</SelectItem>
+                  <SelectItem value="es">Espanõl</SelectItem>
+                  <SelectItem value="fr">Français</SelectItem>
+                </SelectContent>
+              </Select>
+            </li>
+          </ul>
+        </footer>
+      </div>
     </div>
   );
 }
